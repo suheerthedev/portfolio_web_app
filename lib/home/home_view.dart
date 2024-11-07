0f1bb0501fb20dd3f24091128cb541e0a0639e15
Suheer Khan <khansuheer1@gmail.com>
2024-11-07 12:28:21 +0500

    Completed making the design responsive

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -22,7 +22,7 @@ class HomeView extends StatelessWidget {
               children: [
                 _buildHeader(context, model),
                 _buildAboutSection(context),
-                _buildExperienceSection(context),
+                _buildEducationSection(context),
                 _buildProjectsSection(context),
                 _buildFooter(context),
               ],
@@ -119,32 +119,53 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _buildHeader(BuildContext context, HomeViewModel model) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+      width: double.infinity,
       color: Colors.black,
-      child: Column(
-        children: [
-          const CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('assets/images/profile.png'),
+      constraints: BoxConstraints(minHeight: screenHeight * 0.8),
+      padding: EdgeInsets.symmetric(vertical: screenWidth > 1000 ? 50 : 30),
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 800),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/profile.png'),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Suheer Khan!',
+                style: GoogleFonts.poppins(
+                  fontSize: screenWidth > 1000 ? 36 : 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "I flutter and chill",
+                style: GoogleFonts.poppins(
+                  fontSize: screenWidth > 1000 ? 24 : 20,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Enthusiastic Flutter Developer with a commitment to building intuitive and visually appealing mobile applications, specializing in delivering seamless user experiences and efficient solutions.",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  fontSize: screenWidth > 1000 ? 18 : 15,
+                  color: Colors.grey[400],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          Text(
-            'Suheer Khan!',
-            style: GoogleFonts.poppins(
-                fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          Text(
-            "I flutter and chill",
-            style: GoogleFonts.poppins(fontSize: 20, color: Colors.grey),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            "Enthusiastic Flutter Developer with a commitment to building intuitive and visually appealing mobile applications, specializing in delivering seamless user experiences and efficient solutions.",
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(fontSize: 15, color: Colors.grey[400]),
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -168,45 +189,106 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _buildAboutSection(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Text(
-            'ABOUT ME',
-            style:
-                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+      width: double.infinity,
+      color: Colors.grey[900],
+      constraints: BoxConstraints(minHeight: screenHeight * 0.6),
+      child: Center(
+        child: Container(
+          constraints: BoxConstraints(maxWidth: 800),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'ABOUT ME',
+                style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "As an enthusiastic Flutter developer, I enjoy the balance of creativity and logic in app development. Currently focused on building engaging applications, I bring ideas to life by transforming concepts into smooth, responsive user experiences.",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  color: Colors.grey[200],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          Text(
-            """As an enthusiastic Flutter developer, I enjoy the balance of creativity and logic in app development. Currently focused on building engaging applications, I bring ideas to life by transforming concepts into smooth, responsive user experiences.""",
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(fontSize: 15, color: Colors.grey[400]),
-          ),
-        ],
+        ),
       ),
     );
   }
 
-  Widget _buildExperienceSection(BuildContext context) {
+  Widget _buildEducationSection(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
+      width: double.infinity,
       color: Colors.black,
-      padding: const EdgeInsets.all(40),
-      child: Column(
-        children: [
-          Text(
-            'Education',
-            style:
-                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+      constraints: BoxConstraints(minHeight: screenHeight * 0.8),
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 800),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Education / Experience',
+                style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              Container(
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Flutter Development at SMIT',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'May 2024 - Present, Karachi',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      'Currently learning Flutter App Development Application at SMIT. SMIT is an institute with well-trained instructors who have industry experience.',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          _buildEducationItem(
-            title: 'Flutter Development at SMIT',
-            subtitle: 'May 2024 - Present, Karachi',
-            description:
-                'Currently learning Flutter App Development Application at SMIT. SMIT is an institute with well trained instructor who have industry experience.',
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -238,75 +320,121 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _buildProjectsSection(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
-      padding: const EdgeInsets.all(40),
-      child: Column(
-        children: [
-          Text(
-            'PROJECTS',
-            style:
-                GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold),
+      width: double.infinity,
+      color: Colors.grey[900],
+      constraints: BoxConstraints(minHeight: screenHeight * 0.8),
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 1000),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            children: [
+              Text(
+                'PROJECTS',
+                style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Wrap(
+                spacing: 20,
+                runSpacing: 20,
+                alignment: WrapAlignment.center,
+                children: [
+                  _buildProjectTile(
+                      'Newsflix',
+                      'A news app that fetches latest news articles from various sources.',
+                      'assets/images/newsflix_banner.svg'),
+                  _buildProjectTile(
+                      'Weather Now',
+                      'A weather app displaying current weather data based on location.',
+                      'assets/images/weather_banner.svg'),
+                ],
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          LayoutBuilder(
-            builder: (context, constraints) {
-              if (constraints.maxWidth > 600) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _buildProjectTile(
-                        'Newsflix',
-                        'A news app that fetches latest news articles from various sources.',
-                        'assets/images/newsflix_banner.svg'),
-                    _buildProjectTile(
-                        'Weather Now',
-                        'A weather app displaying current weather data based on location.',
-                        'assets/images/weather_banner.svg'),
-                  ],
-                );
-              } else {
-                return Column(
-                  children: [
-                    _buildProjectTile(
-                        'Newsflix',
-                        'A news app that fetches latest news articles from various sources.',
-                        'assets/images/newsflix_banner.svg'),
-                    _buildProjectTile(
-                        'Weather Now',
-                        'A weather app displaying current weather data based on location.',
-                        'assets/images/weather_banner.svg'),
-                  ],
-                );
-              }
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
 
   Widget _buildFooter(BuildContext context) {
     return Container(
+      color: Colors.black,
       padding: const EdgeInsets.all(20),
-      child: Text(
-        'Made with ❤ by Suheer.',
-        style: GoogleFonts.poppins(fontSize: 14, color: Colors.grey[600]),
+      child: Column(
+        children: [
+          Text(
+            'Connect with me',
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.linked_camera, color: Colors.white),
+                onPressed: () =>
+                    _launchURL('https://linkedin.com/in/yourprofile'),
+              ),
+              IconButton(
+                icon: Icon(Icons.photo_camera, color: Colors.white),
+                onPressed: () =>
+                    _launchURL('https://instagram.com/yourprofile'),
+              ),
+              IconButton(
+                icon: Icon(Icons.camera, color: Colors.white),
+                onPressed: () => _launchURL('https://facebook.com/yourprofile'),
+              ),
+              IconButton(
+                icon: Icon(Icons.security, color: Colors.white),
+                onPressed: () => _launchURL('https://github.com/yourprofile'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Text(
+            '© 2024 Suheer Khan. All rights reserved.',
+            style: GoogleFonts.poppins(
+              fontSize: 14,
+              color: Colors.grey[600],
+            ),
+          ),
+        ],
       ),
     );
   }
 
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   Widget _buildProjectTile(
       String title, String description, String bannerPath) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxWidth: 400,
+      ),
       child: Column(
         children: [
           Container(
             constraints: const BoxConstraints(
-              maxWidth: 600,
+              maxWidth: 400,
             ),
             child: AspectRatio(
-              aspectRatio: 4 / 3,
+              aspectRatio: 3 / 2,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(25),
                 child: SvgPicture.asset(
